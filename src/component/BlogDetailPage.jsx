67927df4ee8dfcd653c8028fe76edf7c089c3702
Meet Shah <meetdshah007@gmail.com>
2017-12-08 19:39:45 +0530

    New architecture to work with Node

--- src/component/BlogDetailPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BlogDetailPage = ()=>(
-  <div>
-    Blog Detail Page.
-  </div>
-);
-
-export default BlogDetailPage;
--- src/component/BlogListPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BlogListPage = ()=>(
-  <div>
-    Blog List Page.
-  </div>
-);
-
-export default BlogListPage;
